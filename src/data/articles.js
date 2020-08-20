69e1c148e3333cb5ba9afc2dcb91553baad86362
Juan Dalmasso <j_dalmasso@outlook.com>
2020-08-20 16:26:00 +1000

    Add latest article

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,5 +1,11 @@
 export default [
   {
+    title: 'A practical example of Javascript closures',
+    intro: 'Closures are something that many companies would like you to know before working for them, therefore, chances are they’ll ask you..',
+    url: 'https://medium.com/@juandpr/a-practical-example-of-javascript-closures-d41ce0a7207f',
+    date: 'August 2020'
+  },
+  {
     title: 'Creating a precise countdown with Vanilla JS',
     intro: 'On a recent technical interview I had for a big tech company, in one of the steps of the process I was asked the following...',
     url: 'https://medium.com/@juandpr/creating-a-precise-countdown-with-vanilla-js-cdc44c0483fa',
